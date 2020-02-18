ad0e15a2a242ba8eb286151a0331b8217ddf7e96
Bruncky <brunobrunckhorst@gmail.com>
2020-02-18 17:30:54 +0100

    Added the logic for updating a task + style

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -16,6 +16,21 @@ class TasksController < ApplicationController
     redirect_to tasks_path
   end
 
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(task_params)
+    redirect_to tasks_path
+  end
+
+  def destroy
+    @task.destroy
+    redirect_to tasks_path
+  end
+
   private
 
   def task_params
